f1ec1d5277cbcdf48f4d501bd2ad41284a605fc9
Clement Gizardin <clemgiz@hotmail.fr>
2026-08-18 11:19:31 +0200

    Ajout des routes pour la vue admin

--- projet_full_stack_back/settings.py
+++ projet_full_stack_back/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+7!ls%jz*f^7^w%#ep1#r8gs%)g-hgimovaevv91y=#pfcl=f-'
+SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-+7!ls%jz*f^7^w%#ep1#r8gs%)g-hgimovaevv91y=#pfcl=f-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG','True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 AUTH_USER_MODEL = 'api.User'
@@ -86,12 +89,12 @@ WSGI_APPLICATION = 'projet_full_stack_back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'projet_back',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.mysql'),
+        'NAME': os.getenv('DATABASE_NAME','projet_back'),
+        'USER': os.getenv('DATABASE_USER','root'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD',''),
+        'HOST': os.getenv('DATABASE_HOST','localhost'),
+        'PORT': os.getenv('DATABASE_PORT','3306'),
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
